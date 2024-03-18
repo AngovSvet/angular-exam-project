29a0b2a075c365b6967740c4d1d7c5d990ad8967
Svetoslav <genadiiilarionov@gmail.com>
2024-03-18 13:57:37 +0200

    Added register view

--- exam-app/src/app/user/user.module.ts
+++ exam-app/src/app/user/user.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
-import { UserRoutingModule } from './login/user-routing.module';
+import { UserRoutingModule } from './user-routing.module';
+import { RegisterComponent } from './register/register.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    RouterModule
   ],
-  exports:[LoginComponent]
+  exports:[LoginComponent, RegisterComponent]
 })
 export class UserModule { }
